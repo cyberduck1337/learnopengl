3bfe8293bcfc9ffeea7b74fbc1d74e23599a59b9
cyberduck1337 <cyberduck1337@protonmail.com>
2025-03-04 19:43:49 +0100

    gfx api update

--- Source/Gfx.cpp
+++ Source/Gfx.cpp
@@ -21,6 +21,34 @@ void glfwWindowResizeCallback(GLFWwindow* window, int width, int height)
     }
 }
 
+Gfx::Mesh::Mesh(const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indicies) : m_vertices(vertices), m_indicies(indicies), m_vertexBufferObject(createVertexBufferObject()), m_vertexArrayObject(createVertexArrayObject())
+{
+}
+
+Gfx::Mesh::Mesh(std::vector<Vertex>&& vertices, const std::vector<uint32_t>&& indicies) : m_vertices(std::move(vertices)), m_indicies(std::move(indicies)), m_vertexBufferObject(createVertexBufferObject()), m_vertexArrayObject(createVertexArrayObject())
+{
+}
+
+const std::vector<Gfx::Vertex>& Gfx::Mesh::vertices() const
+{
+    return m_vertices;
+}
+
+const std::vector<uint32_t>& Gfx::Mesh::indicies() const
+{
+    return m_indicies;
+}
+
+Gfx::VertexBufferObjectType Gfx::Mesh::vertexBufferObject() const
+{
+    return m_vertexBufferObject;
+}
+
+Gfx::VertexArrayObjectType Gfx::Mesh::vertexArrayObject() const
+{
+    return m_vertexArrayObject;
+}
+
 void Gfx::initialize(uint32_t width, uint32_t height, const std::string& title, WindowFlags flags)
 {
     KORELIB_VERIFY_THROW(glfwInit() == GLFW_TRUE, korelib::RuntimeException, "Failed to initialize glfw");
@@ -40,11 +68,18 @@ void Gfx::initialize(uint32_t width, uint32_t height, const std::string& title, 
 
     glViewport(0, 0, width, height);
     glfwSetFramebufferSizeCallback(g_window, glfwWindowResizeCallback);
+
+    ShaderType defaultVertexShader = compileShader(DEFAULT_VERTEX_SHADER, ShaderKind::VERTEX);
+    ShaderType defaultFragmentShader = compileShader(DEFAULT_FRAGMENT_SHADER, ShaderKind::FRAGMENT);
+
+    g_defaultShader = linkShaderProgram(defaultVertexShader, defaultFragmentShader);
+    
+    destroyShader(defaultVertexShader);
+    destroyShader(defaultFragmentShader);
 }
 
 void Gfx::beginFrame()
 {
-    Gfx::clearBackground();
 }
 
 bool Gfx::windowShouldClose()
@@ -65,6 +100,122 @@ void Gfx::clearBackground()
 void Gfx::swap()
 {
     glfwSwapBuffers(g_window);
+}
+
+Gfx::VertexBufferObjectType Gfx::createVertexBufferObject()
+{
+    Gfx::VertexBufferObjectType vertexBufferObject{};
+    glGenBuffers(1, &vertexBufferObject);
+
+    return vertexBufferObject;
+}
+
+Gfx::VertexArrayObjectType Gfx::createVertexArrayObject()
+{
+    Gfx::VertexArrayObjectType vertexArrayObject{};
+    glGenVertexArrays(1, &vertexArrayObject);
+
+    return vertexArrayObject;
+}
+
+Gfx::ShaderType Gfx::compileShader(const std::string& source, ShaderKind kind)
+{
+    Gfx::ShaderType shader {0};
+    switch (kind)
+    {
+    case Gfx::ShaderKind::VERTEX:
+        shader = glCreateShader(GL_VERTEX_SHADER);
+        break;
+    case Gfx::ShaderKind::FRAGMENT:
+        shader = glCreateShader(GL_FRAGMENT_SHADER);
+        break;
+    default:
+        KORELIB_VERIFY_THROW(false, korelib::RuntimeException, fmt::format("Unexpected ShaderKind: {}", static_cast<uint8_t>(kind)));
+    }
+
+    const char* shaderSourcePtr = source.data();
+    glShaderSource(shader, 1, &shaderSourcePtr, NULL);
+
+    glCompileShader(shader);
+
+    int  success;
+    char info[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, info);
+        KORELIB_VERIFY_THROW(success, korelib::RuntimeException, fmt::format("Failed to compile shader: {}", info));
+    }
+
+    return shader;
+}
+
+Gfx::ShaderType Gfx::linkShaderProgram(ShaderType vertexShader, ShaderType fragmentShader)
+{
+    Gfx::ShaderType shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    int  success;
+    char info[512];
+    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(shaderProgram, 512, NULL, info);
+        KORELIB_VERIFY_THROW(success, korelib::RuntimeException, fmt::format("Failed to link shader: {}", info));
+    }
+
+    return shaderProgram;
+}
+
+void Gfx::destroyShader(Gfx::ShaderType shader)
+{
+    glDeleteShader(shader);
+}
+
+void Gfx::drawIndexedGeometry(const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indicies, ShaderType shaderProgram, VertexBufferObjectType vertexBufferObject, VertexArrayObjectType vertexArrayObject, const std::vector<Attribute>& attributesDataOffsets)
+{
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject); // 0. select VertexBufferObject to work with
+    glBufferData(GL_ARRAY_BUFFER, sizeof(std::vector<Vertex>::value_type) * vertices.size(), vertices.data(), GL_STATIC_DRAW); // 1. copy vertex data to the video card
+    glBindVertexArray(vertexArrayObject); // 2. select VertexArrayObject to work with
+    for (auto&& attributePointer : attributesDataOffsets) // 3. set the vertex attributes pointers
+    {
+        GLenum attributeType {};
+        switch (attributePointer.type)
+        {
+        case Gfx::Attribute::Type::BYTE:
+            attributeType = GL_BYTE;
+            break;
+        case Gfx::Attribute::Type::UNSIGNED_BYTE:
+            attributeType = GL_UNSIGNED_BYTE;
+            break;
+        case Gfx::Attribute::Type::SHORT:
+            attributeType = GL_SHORT;
+            break;
+        case Gfx::Attribute::Type::UNSIGNED_SHORT:
+            attributeType = GL_UNSIGNED_SHORT;
+            break;
+        case Gfx::Attribute::Type::INTEGER:
+            attributeType = GL_INT;
+            break;
+        case Gfx::Attribute::Type::UNSIGNED_INTEGER:
+            attributeType = GL_UNSIGNED_INT;
+            break;
+        case Gfx::Attribute::Type::FLOAT:
+            attributeType =  GL_FLOAT;
+            break;
+        default:
+            break;
+        }
+
+        glVertexAttribPointer(attributePointer.index, attributePointer.numComponents, attributeType, attributePointer.aligned, attributePointer.stride, (void*)attributePointer.offset);
+        glEnableVertexAttribArray(attributePointer.index);
+    }
+    glUseProgram(Gfx::defaultShaderProgram());
+    glDrawElements(GL_TRIANGLES, indicies.size(), GL_UNSIGNED_INT, indicies.data());
 }
 
 void Gfx::endFrame()
--- Source/Gfx.hpp
+++ Source/Gfx.hpp
@@ -7,16 +7,102 @@
 #include <optional>
 
 #include "Korelib.hpp"
+#include "glm/glm.hpp"
 
 class Gfx final : public korelib::StaticOnlyClass
 {
+public:
     using WindowType = struct GLFWwindow*;
     using WindowReizeDelegate = korelib::Delegate<void, uint32_t, uint32_t>;
+
+    using VertexBufferObjectType = uint32_t;
+
+    static constexpr auto DEFAULT_VERTEX_SHADER = R"(
+        #version 460 core
+        layout (location = 0) in vec3 inPos;
+        layout (location = 1) in vec2 inUV;
+        layout (location = 2) in vec3 inColor;
+
+        out vec3 vertexColor;
+        void main()
+        {
+            gl_Position = vec4(inPos, 1.0);
+            vertexColor = inColor;
+        }
+    )";
+
+    static constexpr auto DEFAULT_FRAGMENT_SHADER = R"(
+        #version 460 core
+        out vec4 FragColor;
+
+        in vec3 vertexColor;
+        void main()
+        {
+            FragColor = vec4(vertexColor, 1.0f);
+        }
+    )";
     
 public:
     enum class WindowFlags : uint32_t
     {
         NONE = 0x00000000
+    };
+
+    struct Vertex
+    {
+        glm::vec3 position;
+        glm::vec2 uv;
+        glm::vec3 color;
+    };
+
+    using VertexBufferObjectType = uint32_t; // stores data
+    using VertexArrayObjectType = uint32_t; // stores pointers in data buffer
+
+    class Mesh
+    {
+    public:
+        Mesh(const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indicies);
+        Mesh(std::vector<Vertex>&& vertices, const std::vector<uint32_t>&& indicies);
+
+        const std::vector<Vertex>& vertices() const;
+        const std::vector<uint32_t>& indicies() const;
+        VertexBufferObjectType vertexBufferObject() const;
+        VertexArrayObjectType vertexArrayObject() const;
+
+    private:
+        std::vector<Vertex> m_vertices;
+        std::vector<uint32_t> m_indicies;
+        VertexBufferObjectType m_vertexBufferObject;
+        VertexArrayObjectType m_vertexArrayObject;
+    };
+    
+    using ShaderType = uint32_t;
+
+    enum class ShaderKind : uint8_t
+    {
+        VERTEX,
+        FRAGMENT
+    };
+
+    struct Attribute
+    {
+        enum class Type : uint32_t
+        {
+            BYTE,
+            UNSIGNED_BYTE,
+            SHORT,
+            UNSIGNED_SHORT,
+            INTEGER,
+            UNSIGNED_INTEGER,
+            FLOAT
+        };
+
+        uint32_t index;
+        uint32_t numComponents;
+        size_t stride;
+        Type type;
+        uintptr_t offset;
+        bool aligned;
     };
 
 public:
@@ -26,6 +112,12 @@ public:
     static void setClearColor(float r, float g, float b, float a);
     static void clearBackground();
     static void swap();
+    static VertexBufferObjectType createVertexBufferObject();
+    static VertexArrayObjectType createVertexArrayObject();
+    static ShaderType compileShader(const std::string& source, ShaderKind kind);
+    static ShaderType linkShaderProgram(ShaderType vertexShader, ShaderType fragmentShader);
+    static void destroyShader(ShaderType shader);
+    static void drawIndexedGeometry(const std::vector<Vertex>& vertecies, const std::vector<uint32_t>& indicies, ShaderType shaderProgram, VertexBufferObjectType vertexBufferObject, VertexArrayObjectType vertexArrayObject, const std::vector<Attribute>& attributesDataOffsets);
     static void endFrame();
     static void destroy();
 
@@ -34,7 +126,13 @@ public:
         return g_onWindowSizeChanged;
     }
 
+    static ShaderType defaultShaderProgram()
+    {
+        return g_defaultShader;
+    }
+
 private:
     static inline WindowType g_window { nullptr };
     static inline WindowReizeDelegate g_onWindowSizeChanged {};
+    static inline ShaderType g_defaultShader {};
 };
