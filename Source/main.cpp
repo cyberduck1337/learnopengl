d78659f13b187cc1476188201620fa160dbfb085
cyberduck1337 <cyberduck1337@protonmail.com>
2025-03-19 00:52:43 +0100

    cube drawing + some imgui debug UI

--- Source/main.cpp
+++ Source/main.cpp
@@ -8,6 +8,8 @@
 
 #include <cstddef>
 #include <vector>
+#include <thread>
+#include <limits>
 
 int main(int argc, char** argv)
 {
@@ -26,17 +28,41 @@ int main(int argc, char** argv)
     });
 
     static Gfx::Mesh mesh{
-        // vertex array [position, uv, color]
+        // vertex array [position, color, uv]
         {
-            {{0.5f,  0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},  // top right
-            {{0.5f, -0.5f, 0.0f},  {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},  // bottom right
-            {{-0.5f, -0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f}},  // bottom left
-            {{-0.5f,  0.5f, 0.0f},  {1.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},  // top left
+        /*[ 0]*/ {{-0.5f, -0.5f,  0.5f},  {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},  // front  - bottom - left
+        /*[ 1]*/ {{-0.5f,  0.5f,  0.5f},  {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},  // front  - top    - left
+        /*[ 2]*/ {{ 0.5f,  0.5f,  0.5f},  {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},  // front  - top    - right
+        /*[ 3]*/ {{ 0.5f, -0.5f,  0.5f},  {1.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},  // front  - bottom - right
+        /*[ 4]*/ {{-0.5f, -0.5f, -0.5f},  {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},  // back   - bottom - left
+        /*[ 5]*/ {{-0.5f,  0.5f, -0.5f},  {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},  // back   - top    - left
+        /*[ 6]*/ {{ 0.5f,  0.5f, -0.5f},  {1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},  // back   - top    - right
+        /*[ 7]*/ {{ 0.5f, -0.5f, -0.5f},  {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},  // back   - bottom - right
+        /*[ 8]*/ {{-0.5f, -0.5f, -0.5f},  {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},  // left   - bottom - back
+        /*[ 9]*/ {{-0.5f,  0.5f, -0.5f},  {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},  // left   - top    - back
+        /*[10]*/ {{-0.5f,  0.5f,  0.5f},  {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},  // left   - top    - front
+        /*[11]*/ {{-0.5f, -0.5f,  0.5f},  {1.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},  // left   - bottom - front
+        /*[12]*/ {{ 0.5f, -0.5f, -0.5f},  {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},  // right  - bottom - back
+        /*[13]*/ {{ 0.5f,  0.5f, -0.5f},  {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},  // right  - top    - back
+        /*[14]*/ {{ 0.5f,  0.5f,  0.5f},  {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},  // right  - top    - front
+        /*[15]*/ {{ 0.5f, -0.5f,  0.5f},  {1.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},  // right  - bottom - front
+        /*[16]*/ {{-0.5f,  0.5f,  0.5f},  {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},  // top    - near   - left
+        /*[17]*/ {{-0.5f,  0.5f, -0.5f},  {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},  // top    - far    - left
+        /*[18]*/ {{ 0.5f,  0.5f, -0.5f},  {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},  // top    - far    - right
+        /*[19]*/ {{ 0.5f,  0.5f,  0.5f},  {1.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},  // top    - near   - right
+        /*[20]*/ {{-0.5f, -0.5f,  0.5f},  {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},  // bottom - near   - left
+        /*[21]*/ {{-0.5f, -0.5f, -0.5f},  {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},  // bottom - far    - left
+        /*[22]*/ {{ 0.5f, -0.5f, -0.5f},  {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},  // bottom - far    - right
+        /*[23]*/ {{ 0.5f, -0.5f,  0.5f},  {1.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},  // bottom - near   - right
         },
         // indicies array
         {
-            0, 1, 3,   // first triangle
-            1, 2, 3    // second triangle
+             2,  1,  0,   0,  3,  2,  // front
+             7,  5,  6,   7,  4,  5,  // back
+             8, 11, 10,   9,  8, 10,  // left
+            15, 12, 13,  13, 14, 15,  // right
+            17, 16, 19,  18, 17, 19,  // top
+            21, 22, 20,  23, 20, 22,  // bottom
         }
     };
 
@@ -67,6 +93,10 @@ int main(int argc, char** argv)
             .aligned = false
         }
     };
+
+    Gfx::Transform& meshTransform = mesh.transform();
+    meshTransform.scale = {1.0f, 1.0f, 1.0f};
+    glm::vec3 rotation = glm::degrees(glm::eulerAngles(meshTransform.rotation));
 
     glm::vec2 mousePosition {};
     glm::vec2 lastMousePosition {};
@@ -108,14 +138,22 @@ int main(int argc, char** argv)
             cam.position() -= cameraSpeed * cam.up();
         }
 
-        Gfx::setShaderProgram(Gfx::defaultShaderProgram());
         cam.update();
+
+        Gfx::setShaderProgram(Gfx::defaultShaderProgram());
 
         Gfx::setShaderMat4x4Value(Gfx::defaultShaderProgram(), "view", cam.view());
         Gfx::setShaderMat4x4Value(Gfx::defaultShaderProgram(), "projection", cam.projection());
 
         Gfx::updateTextureData(texture);
-        Gfx::drawIndexedGeometry(mesh.model(), mesh.vertices(), mesh.indicies(), Gfx::defaultShaderProgram(), mesh.vertexBufferObject(), mesh.vertexArrayObject(), attributes);
+        Gfx::drawIndexedGeometry(mesh.transform(), mesh.vertices(), mesh.indicies(), Gfx::defaultShaderProgram(), mesh.vertexBufferObject(), mesh.vertexArrayObject(), attributes);
+
+        rotation.y += 100 * Gfx::deltaTime();
+        if (rotation.y >= 360.0f)
+        {
+            rotation.y = 0;
+        }
+        meshTransform.rotation = glm::quat(glm::radians(rotation));
 
         mousePosition = Input::GetMousePosition();
 
@@ -127,15 +165,50 @@ int main(int argc, char** argv)
         float& cameraYaw = cam.yaw();
         float& camperaPitch = cam.pitch();
 
-        cameraYaw += xoffset;
-        camperaPitch += yoffset;
-        camperaPitch = glm::clamp(camperaPitch, -89.0f, 89.0f);
+        if (Input::GetMouseButtonDown(GLFW_MOUSE_BUTTON_RIGHT))
+        {
+            cameraYaw += xoffset;
+            camperaPitch += yoffset;
+            camperaPitch = glm::clamp(camperaPitch, -89.0f, 89.0f);
+        }
+
+        float camPos[3] { cam.position().x, cam.position().y, cam.position().z};
+        ImGui::Begin("Camera");
+        if(ImGui::DragFloat3("Position", camPos, 0.1f, std::numeric_limits<float>::lowest(), std::numeric_limits<float>::max()))
+        {
+            cam.position() = {camPos[0], camPos[1], camPos[2]};
+        }
+        ImGui::End();
 
         ImGui::Begin("Mouse");
         ImGui::SliderFloat("Sensetivity", &mouseSensetivity, 0.0f, 1.0f);
         ImGui::End();
 
+        ImGui::Begin("Object");
+        ImGui::InputFloat("Position.x", &meshTransform.position.x);
+        ImGui::InputFloat("Position.y", &meshTransform.position.y);
+        ImGui::InputFloat("Position.z", &meshTransform.position.z);
+        if (ImGui::SliderFloat("Rotation.x", &rotation.x, 0, 359.9f) || ImGui::SliderFloat("Rotation.y", &rotation.y, 0, 359.9f) || ImGui::SliderFloat("Rotation.z", &rotation.z, 0, 359.9f))
+        {
+            meshTransform.rotation = glm::quat(glm::radians(rotation));
+        }
+        ImGui::InputFloat("Scale.x", &meshTransform.scale.x);
+        ImGui::InputFloat("Scale.y", &meshTransform.scale.y);
+        ImGui::InputFloat("Scale.z", &meshTransform.scale.z);
+        ImGui::End();
+
+        ImGui::Begin("UV");
+        size_t index {0};
+        for(Gfx::Vertex& vertex : mesh.vertices())
+        {
+            ImGui::SliderFloat(fmt::format("vert[{}].uv.x", index).c_str(), &vertex.uv.x, 0.0f, 1.0f);
+            ImGui::SliderFloat(fmt::format("vert[{}].uv.y", index).c_str(), &vertex.uv.y, 0.0f, 1.0f);
+            index++;
+        }
+        ImGui::End();
+
         Gfx::endFrame();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 
     Gfx::destroy();
