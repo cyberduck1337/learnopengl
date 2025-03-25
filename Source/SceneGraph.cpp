5016360dd2d835126fff22968f60dd6cd6533af1
cyberduck1337 <cyberduck1337@protonmail.com>
2025-03-25 21:48:11 +0100

    minimal scene graph abstrations like Entity/Scene/GameObject/Component

---
+++ Source/SceneGraph.cpp
@@ -0,0 +1,71 @@
+#include "SceneGraph.hpp"
+#include "Korelib.hpp"
+
+Entity::Entity(const std::string& name, const std::shared_ptr<Entity>& parent) : m_name(name), m_parent(parent)
+{
+}
+
+Entity::Entity(const std::string& name) : Entity(name, nullptr)
+{
+}
+
+const std::string& Entity::getName() const
+{
+    return m_name;
+}
+
+void Entity::setName(const std::string& name)
+{
+    m_name = name;
+}
+
+const std::shared_ptr<Entity>& Entity::getParent() const
+{
+    return m_parent;
+}
+
+void Entity::update()
+{
+    for (auto&& child : m_children)
+    {
+        child->update();
+    }
+}
+
+GameObject::GameObject(const std::string& name, const std::shared_ptr<Entity>& parent) : Entity(name, parent)
+{
+    KORELIB_VERIFY_THROW(parent != nullptr, korelib::RuntimeException, "parent is null");
+    KORELIB_VERIFY_THROW(parent->kind() == Entity::Kind::SCENE || parent->kind() == Entity::Kind::GAME_OBJECT, korelib::RuntimeException, "GameObject parent can be only Entity with type SCENE or GAME_OBJECT");
+}
+
+Component::Component(const std::string& name, const std::shared_ptr<Entity>& parent) : Entity(name, parent)
+{
+    KORELIB_VERIFY_THROW(parent != nullptr, korelib::RuntimeException, "parent is null");
+    KORELIB_VERIFY_THROW(parent->kind() == Entity::Kind::GAME_OBJECT, korelib::RuntimeException, "Component parent can be only Entity with type GAME_OBJECT");
+}
+
+void Component::update()
+{
+}
+
+std::shared_ptr<Scene> Scene::create(const std::string& name)
+{
+    return std::make_shared<Scene>(name);
+}
+
+Scene::Scene(const std::string& name) : Entity(name, nullptr)
+{
+}
+
+std::shared_ptr<GameObject> Scene::addGameObject(const std::string& name, const glm::vec3& position, std::shared_ptr<GameObject> parent)
+{
+    std::shared_ptr<GameObject> go = std::make_shared<GameObject>(name, parent == nullptr ? std::static_pointer_cast<Entity>(shared_from_this()) : parent);
+    go->m_transform.position = position;
+    m_children.emplace_back(go);
+    return std::static_pointer_cast<GameObject>(go);
+}
+
+std::shared_ptr<GameObject> Scene::addGameObject(const std::string& name, const glm::vec3& position)
+{
+    return addGameObject(name, position, nullptr);
+}
---
+++ Source/SceneGraph.hpp
@@ -0,0 +1,114 @@
+#pragma once
+
+#include "ctti/type_id.hpp"
+#include "Korelib.hpp"
+#include "Gfx.hpp"
+#include "glm/glm.hpp"
+
+#include <list>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class Entity : public std::enable_shared_from_this<Entity>
+{
+public:
+    enum class Kind : uint8_t
+    {
+        COMPONENT,
+        GAME_OBJECT,
+        SCENE
+    };
+
+    virtual ~Entity() = default;
+
+    virtual constexpr Kind kind() const = 0;
+    virtual void update();
+
+    const std::string& getName() const;
+    void setName(const std::string& name);
+
+    const std::shared_ptr<Entity>& getParent() const;
+
+protected:
+    Entity(const std::string& name, const std::shared_ptr<Entity>& parent);
+    Entity(const std::string& name);
+
+    void refreshChildIndices();
+
+protected:
+    std::list<std::shared_ptr<Entity>> m_children;
+
+private:
+    std::string m_name;
+    std::shared_ptr<Entity> m_parent;
+};
+
+class GameObject final : public Entity
+{
+public:
+    virtual constexpr Kind kind() const final override
+    {
+        return Kind::GAME_OBJECT;
+    }
+
+    GameObject(const std::string& name, const std::shared_ptr<Entity>& parent);
+
+    template<typename T, typename... TArgs>
+    std::shared_ptr<T> addComponent(TArgs&&... args) requires(std::derived_from<T, class Component>)
+    {
+        ctti::unnamed_type_id_t key = ctti::unnamed_type_id<T>();
+        std::shared_ptr<T> component = std::make_shared<T>(shared_from_this(), std::forward<TArgs>(args)...);
+
+        m_children.emplace_back(component);
+        const std::size_t componentIndex = m_children.size() - 1;
+
+        if(m_componentTypeToIndicesMap.contains(key))
+        {
+            std::vector<std::size_t>& componentsList = m_componentTypeToIndicesMap.at(key);
+            componentsList.emplace_back(componentIndex);
+
+            return component;
+        }
+
+        std::vector<std::size_t> componentList{ componentIndex };
+        m_componentTypeToIndicesMap.insert(std::make_pair(key, std::move(componentList)));
+        return component;
+    }
+
+public:
+    Gfx::Transform m_transform;
+
+protected:
+    std::unordered_map<ctti::unnamed_type_id_t, std::vector<size_t>> m_componentTypeToIndicesMap;
+};
+
+class Component : public Entity
+{
+public:
+    virtual constexpr Kind kind() const final override
+    {
+        return Kind::COMPONENT;
+    }
+
+    virtual void update() override;
+
+protected:
+    Component(const std::string& name, const std::shared_ptr<Entity>& parent);
+};
+
+class Scene final : public Entity
+{
+public:
+    virtual constexpr Kind kind() const final override
+    {
+        return Kind::SCENE;
+    }
+
+    static std::shared_ptr<Scene> create(const std::string& name);
+
+    Scene(const std::string& name);
+    std::shared_ptr<GameObject> addGameObject(const std::string& name, const glm::vec3& position, std::shared_ptr<GameObject> parent);
+    std::shared_ptr<GameObject> addGameObject(const std::string& name, const glm::vec3& position);
+};
